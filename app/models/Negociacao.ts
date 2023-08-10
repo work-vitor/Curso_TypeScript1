2d6d20ef5568341b93d2472ac48916eac1365756
Vitor Rodrigues <joa.fggf3.27@gmail.com>
2023-08-10 00:11:51 -0300

    Modulo 2

--- app/models/Negociacao.ts
+++ app/models/Negociacao.ts
@@ -1,19 +1,29 @@
 export class Negociacao {
+
+    private _data;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
-    get data() {
+
+    get data(){
         return this.data;
     }
-    get quantidade() {
+
+    get quantidade(){
         return this.quantidade;
     }
-    get valor() {
+
+    get valor(){
         return this.valor;
     }
-    get volume() {
+
+    get volume(){
         return this.quantidade * this.valor;
     }
+
 }
