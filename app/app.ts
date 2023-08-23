c81d7ca28b49b3b0771af81c617e702ed832d867
Vitor Rodrigues <joa.fggf3.27@gmail.com>
2023-08-23 01:21:44 -0300

    Modulo 5.1

--- app/app.ts
+++ app/app.ts
@@ -2,8 +2,6 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
 const controller = new NegociacaoController();
 
-console.log('teste');
-
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', event =>{
